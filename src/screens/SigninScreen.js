f642bd20112f8607533065a26616fc04f53ae765
darrylgit <darrylsmithup@gmail.com>
2020-08-29 14:35:53 -0400

    Scaffold signin screen

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 
+import AuthForm from '../components/AuthForm';
+import NavLink from '../components/NavLink';
+
 const SigninScreen = () => {
-  return <Text style={{ fontSize: 48 }}>SigninScreen</Text>;
+  return (
+    <View style={styles.container}>
+      <AuthForm
+        headerText='Sign In to Tracker'
+        errorMessage=''
+        submitButtonText='Sign In'
+        onSubmit={() => {}}
+      />
+      <NavLink
+        text="Don't have an account? Sign up instead"
+        routeName='Signup'
+      />
+    </View>
+  );
 };
 
 SigninScreen.navigationOptions = () => {
@@ -11,6 +27,12 @@ SigninScreen.navigationOptions = () => {
   };
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 125
+  }
+});
 
 export default SigninScreen;
