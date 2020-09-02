cdc779c0ea317d937828f43c39d50cad8b31138d
darrylgit <darrylsmithup@gmail.com>
2020-09-02 11:09:57 -0400

    Create polyline

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,18 +1,19 @@
 import React, { useContext } from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
 import { Button } from 'react-native-elements';
+import { SafeAreaView } from 'react-navigation';
 import Spacer from '../components/Spacer';
 import { Context as AuthContext } from '../contexts/AuthContext';
 
 const AccountScreen = () => {
   const { signOut } = useContext(AuthContext);
   return (
-    <>
+    <SafeAreaView forceInset={{ top: 'always' }}>
       <Text style={{ fontSize: 48 }}>AccountScreen</Text>
       <Spacer>
         <Button title='Sign Out' onPress={signOut} />
       </Spacer>
-    </>
+    </SafeAreaView>
   );
 };
 
