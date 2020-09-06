7c2224cc76e922f80a1ed194480bbe9a7d05368d
darrylgit <darrylsmithup@gmail.com>
2020-09-06 13:57:58 -0400

    Add live location updates

--- src/contexts/LocationContext.js
+++ src/contexts/LocationContext.js
@@ -2,6 +2,8 @@ import createDataContext from './createDataContext';
 
 const locationReducer = (state, action) => {
   switch (action.type) {
+    case 'ADD_CURRENT_LOCATION':
+      return { ...state, currentLocation: action.payload };
     default:
       return state;
   }
@@ -9,7 +11,9 @@ const locationReducer = (state, action) => {
 
 const startRecording = dispatch => () => {};
 const stopRecording = dispatch => () => {};
-const addLocation = dispatch => () => {};
+const addLocation = dispatch => location => {
+  dispatch({ type: 'ADD_CURRENT_LOCATION', payload: location });
+};
 
 export const { Context, Provider } = createDataContext(
   locationReducer,
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,5 +1,5 @@
 import '../_mockLocation';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-navigation';
 import {
@@ -10,8 +10,10 @@ import {
 import { Text } from 'react-native-elements';
 
 import Map from '../components/Map';
+import { Context as LocationContext } from '../contexts/LocationContext';
 
 const TrackCreateScreen = () => {
+  const { addLocation } = useContext(LocationContext);
   const [err, setErr] = useState(null);
 
   const startWatching = async () => {
@@ -27,7 +29,7 @@ const TrackCreateScreen = () => {
           distanceInterval: 10
         },
         location => {
-          console.log(location);
+          addLocation(location);
         }
       );
     } catch (e) {
