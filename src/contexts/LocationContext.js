e4e706fc018c595294666f6ca14a49f16143e8f5
darrylgit <darrylsmithup@gmail.com>
2020-09-07 16:32:32 -0400

    Create actions to update track name and recording status

--- src/contexts/LocationContext.js
+++ src/contexts/LocationContext.js
@@ -4,6 +4,14 @@ const locationReducer = (state, action) => {
   switch (action.type) {
     case 'ADD_CURRENT_LOCATION':
       return { ...state, currentLocation: action.payload };
+    case 'ADD_LOCATION':
+      return { ...state, locations: [...state.locations, action.payload] };
+    case 'CHANGE_NAME':
+      return { ...state, name: action.payload };
+    case 'START_RECORDING':
+      return { ...state, recording: true };
+    case 'STOP_RECORDING':
+      return { ...state, recording: false };
     case 'UPDATE_REGION_CENTER':
       return { ...state, regionCenter: action.payload };
     default:
@@ -11,10 +19,20 @@ const locationReducer = (state, action) => {
   }
 };
 
-const startRecording = dispatch => () => {};
-const stopRecording = dispatch => () => {};
-const addLocation = dispatch => location => {
+const changeName = dispatch => name => {
+  dispatch({ type: 'CHANGE_NAME', payload: name });
+};
+const startRecording = dispatch => () => {
+  dispatch({ type: 'START_RECORDING' });
+};
+const stopRecording = dispatch => () => {
+  dispatch({ type: 'STOP_RECORDING' });
+};
+const addLocation = dispatch => (location, recording) => {
   dispatch({ type: 'ADD_CURRENT_LOCATION', payload: location });
+  if (recording) {
+    dispatch({ type: 'ADD_LOCATION', payload: location });
+  }
 };
 const updateRegionCenter = dispatch => coords => {
   dispatch({ type: 'UPDATE_REGION_CENTER', payload: coords });
@@ -22,8 +40,15 @@ const updateRegionCenter = dispatch => coords => {
 
 export const { Context, Provider } = createDataContext(
   locationReducer,
-  { startRecording, stopRecording, addLocation, updateRegionCenter },
   {
+    startRecording,
+    stopRecording,
+    addLocation,
+    updateRegionCenter,
+    changeName
+  },
+  {
+    name: '',
     recording: false,
     locations: [],
     currentLocation: null,
