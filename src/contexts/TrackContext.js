5c866552ddd8e11d5ad484377124910120e85352
darrylgit <darrylsmithup@gmail.com>
2020-09-09 10:01:03 -0400

    Fetch tracks from database

--- src/contexts/TrackContext.js
+++ src/contexts/TrackContext.js
@@ -3,12 +3,17 @@ import trackerApi from '../api/tracker';
 
 const trackReducer = (state, action) => {
   switch (action.type) {
+    case 'FETCH_TRACKS':
+      return action.payload;
     default:
       return state;
   }
 };
 
-const fetchTracks = dispatch => () => {};
+const fetchTracks = dispatch => async () => {
+  const response = await trackerApi.get('/tracks');
+  dispatch({ type: 'FETCH_TRACKS', payload: response.data });
+};
 const createTrack = dispatch => async (name, locations) => {
   await trackerApi.post('/tracks', { name, locations });
 };
